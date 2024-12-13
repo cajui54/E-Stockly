8d9602b490bb6b38c585c506b86976b1bbf1385e
cajui54 <cajui54@gmail.com>
2024-12-13 17:00:38 -0300

    feat: ensure product stock can be zero

--- app/actions/products/upsert-product/schema.ts
+++ app/actions/products/upsert-product/schema.ts
@@ -11,7 +11,6 @@ export const upsertProductSchema = z.object({
     .min(0.01, { message: 'O preço do produto é obrigatório.' }),
   stock: z.coerce
     .number()
-    .positive({ message: 'Quandidade deve ser maior ou igual a zero' })
     .int()
     .min(0, { message: 'A quantidade em estoque é obrigatório.' }),
 });
