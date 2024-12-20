01dc895b52210b3de4974b18b2620d4d638d47b3
cajui54 <cajui54@gmail.com>
2024-12-20 16:49:32 -0300

    feat: add revenue chart

---
+++ app/(dashboard)/_components/revenue-chart.tsx
@@ -0,0 +1,36 @@
+'use client';
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from '@/app/_components/ui/chart';
+import { DayTotalRevenue } from '@/app/_data-access/dashboard/get-dashboard';
+import { Bar, BarChart, XAxis } from 'recharts';
+
+const chartConfig: ChartConfig = {
+  totalRevenue: {
+    label: 'Receita',
+  },
+};
+interface RevenueCartProps {
+  data: DayTotalRevenue[];
+}
+const RevenueChart = ({ data }: RevenueCartProps) => {
+  return (
+    <ChartContainer config={chartConfig} className="min-h-0 w-full">
+      <BarChart accessibilityLayer data={data}>
+        <XAxis
+          dataKey="day"
+          tickLine={false}
+          tickMargin={10}
+          axisLine={false}
+        />
+        <ChartTooltip content={<ChartTooltipContent />} />
+        <Bar dataKey={'totalRevenue'} radius={5} />
+      </BarChart>
+    </ChartContainer>
+  );
+};
+
+export default RevenueChart;
--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -17,12 +17,19 @@ import {
 } from './_components/summary-card';
 import { getDashboard } from '../_data-access/dashboard/get-dashboard';
 import { formatCurrency } from '../_helpers/currency';
+import RevenueChart from './_components/revenue-chart';
 
 export default async function Home() {
-  const { totalRevenue, todayRevenue, totalSales, totalStock, totalProducts } =
-    await getDashboard();
+  const {
+    totalRevenue,
+    todayRevenue,
+    totalSales,
+    totalStock,
+    totalProducts,
+    totalLast14DaysRevenue,
+  } = await getDashboard();
   return (
-    <div className="space-y8 m-8 w-full rounded-lg">
+    <div className="m-8 w-full space-y-8 rounded-lg">
       <Header>
         <HeaderLeft>
           <HeaderTitle>Dashboard</HeaderTitle>
@@ -75,6 +82,12 @@ export default async function Home() {
           <SummuryCardTitle>Productos</SummuryCardTitle>
           <SummuryCardSubtitle>{totalProducts}</SummuryCardSubtitle>
         </SummaryCard>
+      </div>
+
+      <div className="mt-4 flex h-full flex-col overflow-hidden rounded-xl bg-white p-6">
+        <p className="text-lg font-semibold text-slate-900">Receita</p>
+        <p className="text-sm text-slate-400">Últimos 14 dias</p>
+        <RevenueChart data={totalLast14DaysRevenue} />
       </div>
     </div>
   );
